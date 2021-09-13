a9b2eaaa9de7d3e26068fc08e9a9acbcbf324547
pradeep-selva <pradeepselvaraju@gmail.com>
2021-09-14 02:46:23 +0530

    feature: add quoter in TokenSwap.sol

--- TokenSwap/TokenSwap.sol
+++ TokenSwap/TokenSwap.sol
@@ -4,18 +4,21 @@ pragma abicoder v2;
 
 import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
 import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
+import '@uniswap/v3-periphery/contracts/interfaces/IQuoter.sol';
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
 contract TokenSwap {
     ISwapRouter public immutable swapRouter;
+    IQuoter public immutable quoter;
 
     address public constant oWBTC = 0x379c28627e0D2b219E69511Fd4CB6cFa5Db6D3f1;
     address public constant oWETH = 0x4DB30144d2037E483C442f8FA470Af00E08A6654;
 
     uint24 public constant poolFee = 500;
 
-    constructor(ISwapRouter _swapRouter) {
+    constructor(ISwapRouter _swapRouter, IQuoter _quoter) {
         swapRouter = _swapRouter;
+        quoter = _quoter;
     }
     
     function getBTCBalance() public view returns (uint256) {
@@ -42,5 +45,9 @@ contract TokenSwap {
             });
 
         amountOut = swapRouter.exactInputSingle(params);
+    }
+    
+    function getQuoteExactInputSingle(uint256 amountIn) external returns (uint256 amountOut) {
+        return quoter.quoteExactInputSingle(oWBTC, oWETH, poolFee, amountIn, 0);
     }
 }
